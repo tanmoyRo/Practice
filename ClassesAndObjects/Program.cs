f032cbf451e28c5203e40029373b8c68c79adb62
tanmoyRo <tanmoyroy2108@outlook.com>
2024-07-14 20:16:01 +0530

    FirstCommit

---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ClassesAndObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student _student = new Student();
+            _student.DisplayDetails();
+            Console.ReadKey();
+        }
+    }
+
+    class Student
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? Grade { get; set; }
+        public Student()
+        {
+            Name = "Anonyn";
+            Age = 20;
+            Grade = "A";
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Name: {Name};Age: {Age};Grade:{Grade}");
+        }
+
+
+
+    }
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Course _course1 = new Course();
+            Course _course2 = new Course("ABCD123", "Python", "John Doe");
+            _course1.DisplayDetails();
+            _course2.DisplayDetails();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Course
+    {
+        public string? CourseCode { get; set; }
+        public string? CourseName { get; set; }
+        public string? Instructor { get; set; }
+        public Course()
+        {
+            CourseCode = "CourseCodeAB";
+            CourseName = "C#";
+            Instructor = "Anonyn";
+        }
+
+        public Course(string _courseCode, string _courseName, string _instructor)
+        {
+            CourseCode = _courseCode;
+            CourseName = _courseName;
+            Instructor = _instructor;
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Course Code : {CourseCode}");
+            Console.WriteLine($"Course Name : {CourseName}");
+            Console.WriteLine($"Instructor : {Instructor}");
+        }
+    }
+
+}
